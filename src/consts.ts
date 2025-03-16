57339d733a882c79cbed71dec2cd1d130507c4a3
hy <87596507+Snow0406@users.noreply.github.com>
2025-03-17 03:55:01 +0900

    add: customize-portfolio

--- src/consts.ts
+++ src/consts.ts
@@ -13,25 +13,24 @@ export type Link = {
 }
 
 export const SITE: Site = {
-  TITLE: 'astro-erudite',
-  DESCRIPTION:
-    'astro-erudite is a opinionated, no-frills blogging template—built with Astro, Tailwind, and shadcn/ui.',
-  EMAIL: 'jason@enscribe.dev',
+  TITLE: 'hyuki.dev',
+  DESCRIPTION: 'Game Developer and Incheon National University Student',
+  EMAIL: 'snowland.dev@gmail.com',
   NUM_POSTS_ON_HOMEPAGE: 2,
-  POSTS_PER_PAGE: 3,
-  SITEURL: 'https://astro-erudite.vercel.app',
+  POSTS_PER_PAGE: 4,
+  SITEURL: 'https://hyuki.dev',
 }
 
 export const NAV_LINKS: Link[] = [
+  { href: '/', label: 'home' },
   { href: '/blog', label: 'blog' },
-  { href: '/authors', label: 'authors' },
-  { href: '/about', label: 'about' },
+  { href: '/project', label: 'project' },
   { href: '/tags', label: 'tags' },
 ]
 
 export const SOCIAL_LINKS: Link[] = [
-  { href: 'https://github.com/jktrn', label: 'GitHub' },
-  { href: 'https://twitter.com/enscry', label: 'Twitter' },
-  { href: 'jason@enscribe.dev', label: 'Email' },
+  { href: 'https://github.com/snow0406', label: 'GitHub' },
+  { href: 'https://twitter.com/snowflake597', label: 'Twitter' },
+  { href: 'snowland.dev@gmail.com', label: 'Email' },
   { href: '/rss.xml', label: 'RSS' },
 ]
