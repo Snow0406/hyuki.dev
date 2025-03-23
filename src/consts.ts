eadee732ae9445dc8ce3671f6680f650c144651c
hy <87596507+Snow0406@users.noreply.github.com>
2025-03-23 19:56:40 +0900

    chore: update template source to latest version

--- src/consts.ts
+++ src/consts.ts
@@ -1,36 +1,32 @@
-export type Site = {
-  TITLE: string
-  DESCRIPTION: string
-  EMAIL: string
-  NUM_POSTS_ON_HOMEPAGE: number
-  POSTS_PER_PAGE: number
-  SITEURL: string
-}
-
-export type Link = {
-  href: string
-  label: string
-}
+import type { IconMap, SocialLink, Site } from '@/types'
 
 export const SITE: Site = {
-  TITLE: 'hyuki.dev',
-  DESCRIPTION: 'Game Developer and Incheon National University Student',
-  EMAIL: 'snow@hyuki.dev',
-  NUM_POSTS_ON_HOMEPAGE: 2,
-  POSTS_PER_PAGE: 4,
-  SITEURL: 'https://hyuki.dev',
+  title: 'hyuki.dev',
+  description: 'Game Developer and Incheon National University Student',
+  href: 'https://hyuki.dev',
+  featuredPostCount: 1,
+  postsPerPage: 3,
 }
 
-export const NAV_LINKS: Link[] = [
+export const NAV_LINKS: SocialLink[] = [
   { href: '/', label: 'home' },
   { href: '/blog', label: 'blog' },
   { href: '/project', label: 'project' },
   { href: '/tags', label: 'tags' },
 ]
 
-export const SOCIAL_LINKS: Link[] = [
+export const SOCIAL_LINKS: SocialLink[] = [
   { href: 'https://github.com/snow0406', label: 'GitHub' },
   { href: 'https://twitter.com/snowflake597', label: 'Twitter' },
   { href: 'snowland.dev@gmail.com', label: 'Email' },
   { href: '/rss.xml', label: 'RSS' },
 ]
+
+export const ICON_MAP: IconMap = {
+  Website: 'lucide:globe',
+  GitHub: 'lucide:github',
+  LinkedIn: 'lucide:linkedin',
+  Twitter: 'lucide:twitter',
+  Email: 'lucide:mail',
+  RSS: 'lucide:rss',
+}
